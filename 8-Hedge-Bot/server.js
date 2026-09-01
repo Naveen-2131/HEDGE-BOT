f8847d53679c18b17dffa56834e70f81a57820b3
Naveen <naveen8870002032@gmail.com>
2026-09-02 00:52:21 +0530

    Update server.js

--- 8-Hedge-Bot/server.js
+++ 8-Hedge-Bot/server.js
@@ -6,6 +6,7 @@
  */
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const publicDir = path.join(__dirname, 'public');
